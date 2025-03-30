73f9dd8e710e32ee92dd94d54789367312705eea
Kheirolegoat <155398639+Kheirolegoat@users.noreply.github.com>
2025-03-30 02:58:49 +0100

    Mise a jour de PenduDrawer
    Ajout methode drawFull

---
+++ PenduDrawer.java
@@ -0,0 +1,51 @@
+package Eureka.Controller;
+
+import java.util.List;
+import javafx.scene.image.ImageView;
+
+public class PenduDrawer {
+    private final List<ImageView> bodyParts;
+    private int attemptsLeft;
+
+    public PenduDrawer(List<ImageView> bodyParts, int maxAttempts) {
+        this.bodyParts = bodyParts;
+        this.attemptsLeft = maxAttempts;
+        reset();
+    }
+
+    public void drawNextPart() {
+        int i = bodyParts.size() - attemptsLeft - 1;
+        if (i >= 0 && i < bodyParts.size()) {
+            bodyParts.get(i).setVisible(true);
+        }
+    }
+
+    public void reset() {
+        for (ImageView part : bodyParts) {
+            part.setVisible(false);
+        }
+        attemptsLeft = bodyParts.size();
+    }
+
+    public void setAttemptsLeft(int attempts) {
+        this.attemptsLeft = attempts;
+        drawNextPart();
+    }
+
+    public int getAttemptsLeft() {
+        return attemptsLeft;
+    }
+
+    public boolean isGameOver() {
+        return attemptsLeft <= 0;
+    }
+
+    // Affiche toutes les parties du corps d'un coup (mode survie)
+    public void drawFull() {
+        for (ImageView part : bodyParts) {
+            part.setVisible(true);
+        }
+        this.attemptsLeft = 0;
+    }
+}
+
