bf9b8d9c9d2eda6bc893397a235da61163c9f4b3
Kheirolegoat <155398639+Kheirolegoat@users.noreply.github.com>
2025-04-11 21:35:15 +0100

    GameOverController.java update setScore

--- GameOverController.java
+++ GameOverController.java
@@ -1,5 +1,7 @@
 package Eureka.Controller;
 
+import Eureka.models.Badge;
+import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,13 +13,13 @@ public class GameOverController {
     private AnchorPane root;
 
     @FXML
-    private Button btn_again, btn_leave;
+    private Button btn_again, btn_leave, btn_revision;
 
     @FXML
     private Label lbl_score;
 
     @FXML
-    private Label lbl_message; // Optionnel pour message "Joueur 1 a perdu"
+    private Label lbl_message, lbl_badge; // Optionnel pour message "Joueur 1 a perdu"
 
     public void initialize() {
         SoundEffects.addSound(btn_again);
@@ -32,11 +34,28 @@ public class GameOverController {
             SoundEffects.clickSound.play();
             DbController.changeScene(e, "Loggedin.fxml");
         });
+
+        btn_revision.setOnAction(e -> {
+            SoundEffects.clickSound.play(); 
+            DbController.changeScene(e, "ModeRevision.fxml");
+        });
     }
 
-    public void setScore(int score) {
+    public void setScore(int score, List<Badge> newBadges) {
         lbl_score.setText(String.valueOf(score));
+    
+        if (newBadges != null && !newBadges.isEmpty()) {
+            String msg = "🎉 Tu as gagné " + newBadges.size() + " badge";
+            if (newBadges.size() > 1) msg += "s";
+            msg += " !";
+            lbl_badge.setText(msg);
+        } else {
+            lbl_badge.setText(""); 
+        }
     }
+    
+    
+
 
     public void setGameOverMessage(String message) {
         if (lbl_message != null) {
